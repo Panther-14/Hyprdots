43f310943ec547f088082cd07b2627866df05275
Panther-14 <trabajosjuanpa@gmail.com>
2025-01-08 20:18:43 -0600

    Adding Files

--- .config/hypr/magic/hyprPicker.sh
+++ .config/hypr/magic/hyprPicker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 hyprpicker --format hex | head -c -1 | wl-copy
 magick -size 100x100 xc:$(wl-paste) /tmp/color.png
-notify-send --icon=/tmp/color.png "$(wl-paste)"  "Copied to your clipboard!"
+notify-send --app-name="Color Picker" --icon=/tmp/color.png "$(wl-paste)"  "Copied to your clipboard!"
